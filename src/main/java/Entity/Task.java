f9ae9e1cac510951a056ef31a3a04136c4dd4a9a
Ezequiel Morais <ezequielmorais4002@gmail.com>
2023-06-25 18:49:00 -0300

    completada classe Task

--- src/main/java/Entity/Task.java
+++ src/main/java/Entity/Task.java
@@ -3,9 +3,26 @@ package Entity;
 import Controller.TaskControllerAbstract;
 
 public abstract class Task {
+    private String name;
+    private int id;
     private String type;
     private TaskControllerAbstract controller;
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
     public String getType() {
         return type;
     }
