c3414b16db5885a5af0362e70955cf2630c95c36
Ezequiel Morais <ezequielmorais4002@gmail.com>
2023-06-25 18:28:26 -0300

    terminada classe project

--- src/main/java/Entity/Project.java
+++ src/main/java/Entity/Project.java
@@ -3,7 +3,30 @@ package Entity;
 import Controller.ProjectControllerAbstract;
 
 public abstract class Project {
-    public ProjectControllerAbstract controller;
+    private String name;
+    private int id;
+    private ProjectControllerAbstract controller;
+
+    public void Project(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public abstract Object setMetrics();
 }
