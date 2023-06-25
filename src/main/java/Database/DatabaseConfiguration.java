3fbdee3a8b9ac214df9949580be665e5e38d4cb8
Ezequiel Morais <ezequielmorais4002@gmail.com>
2023-06-25 19:10:56 -0300

    Atualizados atributos da classe DatabaseConfiguration

--- src/main/java/Database/DatabaseConfiguration.java
+++ src/main/java/Database/DatabaseConfiguration.java
@@ -3,5 +3,8 @@ package Database;
 import java.sql.Connection;
 
 public abstract class DatabaseConfiguration {
+    String username;
+    String password;
+    String table;
     public abstract Connection connect();
 }
