a66291232e5dbab730997a91deca767c611a2a2e
Ezequiel Morais <ezequielmorais4002@gmail.com>
2023-06-25 19:22:25 -0300

    criado DAO de projeto

--- src/main/java/Dao/ProjectDAO.java
+++ src/main/java/Dao/ProjectDAO.java
@@ -1,23 +1,97 @@
 package Dao;
 
-public class ProjectDAO implements DAO{
+import Database.DatabaseConfiguration;
+import Entity.Project;
+import Entity.Project;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ProjectDAO implements ProjectDAOInterface{
+    Connection con;
+    DatabaseConfiguration config;
+
+    public ProjectDAO(DatabaseConfiguration DBConfig){
+        this.config = DBConfig;
+        con = config.connect();
+    }
+
+    public Project buildProject(Project project, ResultSet rs) throws SQLException {
+        project.setName(rs.getString("name"));
+        project.setId(rs.getInt("id"));
+        return project;
+    }
+
+    public PreparedStatement buildFullStatement(PreparedStatement pst, Project project) throws SQLException{
+        pst.setString(1, project.getName());
+        pst.setInt(4, project.getId());
+        return pst;
+    }
+
+    public boolean create(Project project) throws SQLException{
+        String query = "INSERT INTO " + config.getTable() + " (name, email, password, id) VALUES (?,?,?,?)";
+        PreparedStatement pst;
+        pst = con.prepareStatement(query);
+        pst = buildFullStatement(pst, project);
+
+        int res = pst.executeUpdate();
+
+        if (res == 1) {
+            return true;
+        }
+
+        return false;
+    }
     @Override
-    public boolean create() {
+    public boolean delete(int id) throws SQLException {
+        String string = "DELETE FROM " + config.getTable() + " WHERE id = ?";
+        PreparedStatement pst;
+        pst = con.prepareStatement(string);
+        pst.setInt(1, id);
+
+        int res = pst.executeUpdate();
+
+        if(res == 1){
+            return true;
+        }
+
         return false;
     }
 
     @Override
-    public boolean read() {
+    public boolean update(Project project) {
         return false;
     }
 
-    @Override
-    public boolean update() {
-        return false;
-    }
+    public Project getProjectById(Project project) throws SQLException{
+        String query = "SELECT * FROM " + config.getTable();
+        PreparedStatement ps;
+        ps = con.prepareStatement(query);
+        ResultSet rs = ps.executeQuery();
 
-    @Override
-    public boolean delete() {
+        while (rs.next()) {
+            Project returnProject = buildProject(project, rs);
+            if(returnProject.getId() == project.getId()) {
+                return returnProject;
+            }
+
+        }
+        return null;
+    }
+    public boolean update(Project project, String entityName) throws SQLException{
+        String query = "UPDATE project SET " + entityName + " = ?, email = ?, password = ? WHERE id = ?";
+        PreparedStatement pst;
+        pst = con.prepareStatement(query);
+        pst = buildFullStatement(pst, project);
+
+        int res = pst.executeUpdate();
+
+        if(res == 1){
+            return true;
+        }
+
         return false;
     }
 }
