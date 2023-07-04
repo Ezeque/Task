b7bb83217b6329e2d61bd145fa97a1a69a6ee1bb
unknown <nandagb23@gmail.com>
2023-07-04 13:30:38 -0300

    relatorio service

--- src/main/java/service/RelatorioAlunoService.java
+++ src/main/java/service/RelatorioAlunoService.java
@@ -1,2 +1,32 @@
-package service;public class RelatorioAlunoService {
+package service;
+
+import database.DBConfigAluno;
+import tfw.Entity.UserReport;
+import tfw.Service.UserReportService;
+
+public class RelatorioAlunoService extends UserReportService {
+
+    public RelatorioAlunoService(DBConfigAluno dbConfig) {
+        super(dbConfig);
+    }
+
+    @Override
+    public boolean validateCreation(UserReport report) {
+        return true;
+    }
+
+    @Override
+    public boolean validateSearch(UserReport report) {
+        return true;
+    }
+
+    @Override
+    public boolean validateUpdate(UserReport report) {
+        return true;
+    }
+
+    @Override
+    public boolean validateDeletion(UserReport report) {
+        return true;
+    }
 }
