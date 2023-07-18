f22e454833343d85eb0fa6a1bc61836e5f1671b6
Ezequiel Morais <ezequielmorais4002@gmail.com>
2023-07-18 20:38:40 -0300

    adicionado setStructure e structure no framework

--- src/main/java/tfw/Entity/UserReport.java
+++ src/main/java/tfw/Entity/UserReport.java
@@ -1,12 +1,18 @@
 package tfw.Entity;
 
+import java.util.Map;
+
 public abstract class UserReport {
     private int id;
     private int user_id;
 
+    String structure;
+
     public UserReport() {
 
     }
+
+    public abstract void setStructure(Map<String, Integer> metrics);
 
     public UserReport(int user_id) {
         this.user_id = user_id;
